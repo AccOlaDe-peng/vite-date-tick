6994cf4617b4628dda644c3a9b7061c9c456bb57
pengrenchang <pengrch@activeio.com.cn>
2024-08-30 18:30:07 +0800

    fix: 预研初始完成

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+/*
+ * @description:
+ * @author: pengrenchang
+ * @Date: 2024-08-14 10:30:14
+ * @LastEditors: pengrenchang
+ * @LastEditTime: 2024-08-30 10:28:15
+ */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        port: 9999,
+    },
 });
